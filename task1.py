7bd1f3a00c3336854c93d751a266a2c9a7f859ca
Arjunbabu15 <113515831+Arjunbabu15@users.noreply.github.com>
2023-01-10 23:36:00 +0530

    Update and rename python-task to task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+#Python if else, for loop, and range() Exercises 
+
+  #Exercise 1: Print First 10 natural numbers using while loop
+
+num =0
+
+while num<=10:
+    print(num)
+    num= num+1
+    
+  

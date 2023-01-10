a6efc8b34d1e2bc70c43b89485af843d9488dabf
Arjunbabu15 <113515831+Arjunbabu15@users.noreply.github.com>
2023-01-10 23:49:23 +0530

    Create task3_3.py

---
+++ task3_3.py
@@ -0,0 +1,8 @@
+#Exercise 3: Turn every item of a list into its square
+#numbers = [1, 2, 3, 4, 5, 6, 7]
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+result = []
+for i in numbers:
+result.append(i * i)
+print(result)

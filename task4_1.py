0030abdfdaff37b9239f06a0122f61b3561a6abd
Arjunbabu15 <113515831+Arjunbabu15@users.noreply.github.com>
2023-01-10 23:52:09 +0530

    Create task4_1.py

---
+++ task4_1.py
@@ -0,0 +1,10 @@
+#Python Input and Output Exercise
+
+#Exercise 1: Accept numbers from a user
+#Write a program to accept two numbers from the user and calculate multiplication
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = num1 * num2
+print("Multiplication result is:", result)

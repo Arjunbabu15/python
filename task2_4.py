213fc0e78d66d112e911ad2efcafeeca06dda173
Arjunbabu15 <113515831+Arjunbabu15@users.noreply.github.com>
2023-01-10 23:45:34 +0530

    Create task2_4.py

---
+++ task2_4.py
@@ -0,0 +1,7 @@
+#Exercise 4: Find the last position of a given substring
+
+str = "Emma is a data scientist who knows Python. Emma works at google."
+print("Original String is:", str)
+
+index = str.rfind("Emma")
+print("Last occurrence of Emma starts at index:", index)
